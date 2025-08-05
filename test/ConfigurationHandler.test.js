43424f1e216b01da1871955e746a745a31fd59df
C043 <mariofragnitoph@gmail.com>
2025-08-05 10:54:08 +0200

    Change format

--- test/ConfigurationHandler.test.js
+++ test/ConfigurationHandler.test.js
@@ -1,82 +1,79 @@
-import sinon from "sinon";
-import fs from "fs";
-import jsonfile from "jsonfile";
-import ConfigurationHandler from "../src/ConfigurationHandler.js";
-import assert from "assert";
-import * as inquirer from "@inquirer/prompts";
+import sinon from "sinon"
+import fs from "fs"
+import jsonfile from "jsonfile"
+import ConfigurationHandler from "../src/ConfigurationHandler.js"
+import assert from "assert"
 
 describe("ConfigurationHandler", () => {
-  let sandbox;
+  let sandbox
 
   beforeEach(() => {
-    sandbox = sinon.createSandbox();
-  });
+    sandbox = sinon.createSandbox()
+  })
 
   afterEach(() => {
-    sandbox.restore();
-  });
+    sandbox.restore()
+  })
 
   it("should create config file if missing", () => {
-    const existisSyncStub = sandbox.stub(fs, "existsSync");
-    const mkdirSyncStub = sandbox.stub(fs, "mkdirSync");
-    const writeFileSyncStub = sandbox.stub(fs, "writeFileSync");
+    const existisSyncStub = sandbox.stub(fs, "existsSync")
+    const mkdirSyncStub = sandbox.stub(fs, "mkdirSync")
+    const writeFileSyncStub = sandbox.stub(fs, "writeFileSync")
     const readFileSyncStub = sandbox
       .stub(jsonfile, "readFileSync")
-      .returns({ key: "", model: "gpt-4o" });
+      .returns({ key: "", model: "gpt-4o" })
 
-    existisSyncStub.onCall(0).returns(false);
-    existisSyncStub.onCall(1).returns(false);
+    existisSyncStub.onCall(0).returns(false)
+    existisSyncStub.onCall(1).returns(false)
 
-    const logStub = sandbox.stub(console, "log");
+    const logStub = sandbox.stub(console, "log")
 
-    const handler = new ConfigurationHandler();
+    const handler = new ConfigurationHandler()
 
     sinon.assert.calledWith(mkdirSyncStub, sinon.match.string, {
       recursive: true
-    });
+    })
 
     sinon.assert.calledWith(
       writeFileSyncStub,
       sinon.match.string,
       sinon.match.string,
       "utf8"
-    );
+    )
 
-    sinon.assert.called(logStub);
-    assert.deepStrictEqual(handler.getConfig(), { key: "", model: "gpt-4o" });
-  });
+    sinon.assert.called(logStub)
+    assert.deepStrictEqual(handler.getConfig(), { key: "", model: "gpt-4o" })
+  })
 
   it("should load config if file exists", () => {
-    sandbox.stub(fs, "existsSync").returns(true);
+    sandbox.stub(fs, "existsSync").returns(true)
     sandbox
       .stub(jsonfile, "readFileSync")
-      .returns({ key: "abc", model: "gpt-3o" });
+      .returns({ key: "abc", model: "gpt-3o" })
 
-    const handler = new ConfigurationHandler();
+    const handler = new ConfigurationHandler()
 
     assert.deepStrictEqual(handler.getConfig(), {
       key: "abc",
       model: "gpt-3o"
-    });
-    assert.strictEqual(handler.get("key"), "abc");
-  });
+    })
+    assert.strictEqual(handler.get("key"), "abc")
+  })
 
   it("should save config when set() is called", () => {
-    sandbox.stub(fs, "existsSync").returns(true);
-    sandbox
-      .stub(jsonfile, "readFileSync")
-      .returns({ key: "", model: "gpt-4o" });
-    const writeStub = sandbox.stub(jsonfile, "writeFileSync");
+    sandbox.stub(fs, "existsSync").returns(true)
+    sandbox.stub(jsonfile, "readFileSync").returns({ key: "", model: "gpt-4o" })
+    const writeStub = sandbox.stub(jsonfile, "writeFileSync")
 
-    const handler = new ConfigurationHandler();
+    const handler = new ConfigurationHandler()
 
-    assert.strictEqual(handler.get("key"), "");
+    assert.strictEqual(handler.get("key"), "")
 
-    handler.set("key", "newKey");
+    handler.set("key", "newKey")
 
-    assert.strictEqual(handler.get("key"), "newKey");
+    assert.strictEqual(handler.get("key"), "newKey")
     sinon.assert.calledWith(writeStub, handler.filePath, handler.config, {
       spaces: 2
-    });
-  });
-});
+    })
+  })
+})
